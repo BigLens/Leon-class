6f60635064c965f45490211d7564524322b41a6f
BigLens <adedejibioku@gmail.com>
2024-05-08 05:11:24 +0100

    done for now

--- class19-materials/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,5 +1,11 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
+const movieTitles = ['Unexpected places', 'Beekeeper', 'Avengers', 'JagunJagun'];
+//Looping through the array
+for (let i = 0; i < movieTitles.length; i++){
+	document.querySelector('h2').innerText = movieTitles[i];
+}
+
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 
