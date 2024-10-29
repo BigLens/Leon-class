6205ff3f27e3991c60955879b1f8f23e4342e6d9
BigLens <adedejibioku@gmail.com>
2024-10-29 19:58:27 +0100

    committing

--- warGame/war-game.js
+++ warGame/war-game.js
@@ -1,9 +1,11 @@
-if(){
-
+if(!localStorage.getItem('getCard'){
+	localStorage.setItem('getCard', 0);
 }
 
 document.querySelector('button').addEventListener('click', addCard);
 
 function addCard(){
-
+	let appendCard = Number(localStorage.getItem('getCard'));
+	appendCard += 1;
+	localStorage.setItem('getCard', appendCard);
 }
