68a932d8613da173b2f8a7b7497526a566be0d28
BigLens <adedejibioku@gmail.com>
2025-05-01 23:16:57 +0100

    new directory added

---
+++ practice/inheritance.js
@@ -0,0 +1,18 @@
+class Animal {
+	constructor(name) {
+		this.name = name
+	}
+
+	speak(){
+		console.log(`${this.name} makes a sound`)
+	}
+}
+
+class Dog extends Animal{
+	constructor(name, breed){
+		super(name)
+		this.breed = breed;
+	}
+}
+let simba = new Dog('simba', 'shephard')
+simba.speak();
